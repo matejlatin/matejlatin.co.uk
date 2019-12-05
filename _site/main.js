91fb9311bdf0fb8e8cbb11e64e4a42590d068c94
matejlatin <mlatin@gitlab.com>
2019-12-05 16:06:25 +0100

    Switch to mailerlite

--- _site/main.js
+++ _site/main.js
@@ -1,11 +1,12 @@
 $(document).on('submit', 'form', function(e) {
   var $this = $(this);
   $.ajax({
-    type: "GET",
-    url: 'https://betterwebtype.us2.list-manage.com/subscribe/post-json?c=?',
+    // type: "GET",
+    url: 'https://api.mailerlite.com/api/v2/groups/3640549/subscribers',
     data: $this.serialize(),
     dataType: 'json',
     contentType: "application/json; charset=utf-8",
+    headers: { "X-MailerLite-ApiKey": "3c9772b7ecfd84d3c5981afb78d6423e" },
     error: function(err) {
           // $('article, footer').addClass('blurEffect');
           $('#msg').fadeIn(300);
@@ -19,11 +20,13 @@ $(document).on('submit', 'form', function(e) {
         success: function(data) {
           if (data.result != "success") {
               // $('article, footer').addClass('blurEffect');
+            console.log(data);
               $('#msg').fadeIn(300);
               var msg = data.msg;
               var msgReadable = msg.replace('0 -', '')
               $('#msgContent').empty().append('<h2 class="bold">Something Went Wrong.</h2><p>' + msgReadable + '<p/><a href="javascript:void(0)" id="btnClose" class="button">OK, got it</a>').removeClass().addClass('animated zoomIn');
             } else {
+              console.log(data);
               window.location.href = "/almost-finished";
             }
             $('#btnClose').on('click', function(){
